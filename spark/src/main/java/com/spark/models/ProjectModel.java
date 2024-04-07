d61f3594033d08b95d4528c0e7df7064b2d19022
Desgarrosa <djrruano@gmail.com>
2024-04-07 03:49:10 +0100

    feat: added ProjectModel parameters with getters/setters

--- spark/src/main/java/com/spark/models/ProjectModel.java
+++ spark/src/main/java/com/spark/models/ProjectModel.java
@@ -1,7 +1,23 @@
 package com.spark.models;
 
+import lombok.Data;
+
+@Data
 public class ProjectModel {
+    private final String projectName;
+    private final String projectDescription;
+    private final String[] projectDeliverables;
+    private final String completionDate;
+    private final String[] categories;
+    private final String structureOverview;
 
-    
 
+    public ProjectModel(String projectName, String projectDescription, String[] projectDeliverables, String completionDate, String[] categories, String structureOverview) {
+        this.projectName = projectName;
+        this.projectDescription = projectDescription;
+        this.projectDeliverables = projectDeliverables;
+        this.completionDate = completionDate;
+        this.categories = categories;
+        this.structureOverview = structureOverview;
+    }
 }
